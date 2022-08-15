8ea419b12dcf65daff5855ed46db8b98d7f7f302
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-08-15 03:36:09 -0500

    Created login screen with MaterialUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,17 @@
 import './App.css';
-import Header from './components/Header/Header';
+
 import { HomeScreen } from './screens/HomeScreen/HomeScreen';
-import { LoginScreen } from './screens/LoginScreen/LoginScreen';
-import { Routes, Route, Link } from 'react-router-dom';
+import LoginScreen from './screens/LoginScreen/LoginScreen';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={LoginScreen} />
-        <Route path="/home" element={HomeScreen} />
-        <Route path="/home/suscribers" element={<p>SUSCRIBERS</p>} />
+        <Route path="/" element={<LoginScreen />} />
+        <Route path="/home" element={<HomeScreen />} />
+        <Route path="/suscribers" element={<p>SUSCRIBERS</p>} />
       </Routes>
-      <Header />
-      <HomeScreen />
     </div>
   );
 }
