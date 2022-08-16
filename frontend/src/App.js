b7a1a7227f33b778380adcba2d7a39c137563094
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-08-16 16:56:44 -0500

    fixed lf issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { HomeScreen } from './screens/HomeScreen/HomeScreen';
 import LoginScreen from './screens/LoginScreen/LoginScreen';
 import { Routes, Route } from 'react-router-dom';
+import { ListSuscriptionsScreen } from './screens/ListSuscriptionsScreen';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LoginScreen />} />
         <Route path="/home" element={<HomeScreen />} />
         <Route path="/suscribers" element={<p>SUSCRIBERS</p>} />
+        <Route path="/list-suscriptions" element={<ListSuscriptionsScreen />} />
       </Routes>
     </div>
   );
